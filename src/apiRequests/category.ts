cab7a8b63d92c130aecbf4eb02288ce2a703f385
voconghuy3011 <voconghuy3011@outlook.com.vn>
2025-03-20 12:29:46 +0700

    fix vercel build

--- src/apiRequests/category.ts
+++
@@ -1,9 +0,0 @@
-import { apiEndpoint } from "@/configs/routes";
-import http from "@/lib/http";
-
-const categoryApiRequest = {
-  get: () =>
-    http.get(apiEndpoint.categoryOdata),
-};
-
-export default categoryApiRequest;
